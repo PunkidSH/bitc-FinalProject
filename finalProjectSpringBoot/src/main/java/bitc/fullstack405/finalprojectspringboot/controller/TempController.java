279d202d82b09e6c88eee3d690509793dba1ae86
PunkidSH <940809psh@naver.com>
2024-10-02 19:42:58 +0900

    Spring Security 미완성본

--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/controller/TempController.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/controller/TempController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin("http://localhost:5173")
+//@CrossOrigin("http://localhost:5173")
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
--- finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/service/UserDetailsService.java
+++ finalProjectSpringBoot/src/main/java/bitc/fullstack405/finalprojectspringboot/service/UserDetailsService.java
@@ -1,14 +1,18 @@
 package bitc.fullstack405.finalprojectspringboot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+//implements org.springframework.security.core.userdetails.UserDetailsService // 클래스 활성화 시 해당 내용 삽입
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsService {
+public class UserDetailsService  {
 
-//  private final AdminRepository adminRepository;
-
+//  private final UserRepository userRepository;
+//
 //  @Override
 //  public UserDetails loadUserByUsername(String userAccount) throws UsernameNotFoundException {
 //    userEntity user = adminRepository.findByUserAccount(userAccount).orElseThrow(() -> newUsernameNotFoundException(userAccount + " 는 등록되지 않은 계정입니다."));
